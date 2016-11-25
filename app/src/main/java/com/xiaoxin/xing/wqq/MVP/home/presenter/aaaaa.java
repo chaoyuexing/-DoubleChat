3e2ce5fa07639548b63c29977bffc471f3d76fd2
chaoyuexing <931368239@qq.com>
2016-11-25 10:56:06 +0800

    解决分包问题111,这些类是无用的

---
+++ app/src/main/java/com/xiaoxin/xing/wqq/MVP/home/presenter/aaaaa.java
@@ -0,0 +1,9 @@
+package com.xiaoxin.xing.wqq.MVP.home.presenter;
+
+/**
+ * Created time 2016/11/25.
+ * Created name xing.
+ */
+
+public class aaaaa {
+}
---
+++ app/src/main/java/com/xiaoxin/xing/wqq/MVP/home/view/aaaaaaaa.java
@@ -0,0 +1,9 @@
+package com.xiaoxin.xing.wqq.MVP.home.view;
+
+/**
+ * Created time 2016/11/25.
+ * Created name xing.
+ */
+
+public class aaaaaaaa {
+}
